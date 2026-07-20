c6c8ac0aee1539b7150c2422e0d8c2e699d821d5
Alain31140 <danes_alain@hotmail.com>
2026-07-20 14:26:33 +0200

    config

---
+++ js/config.js
@@ -0,0 +1,12 @@
+/* ==================================================
+   CONFIGURATION GLOBALE
+================================================== */
+
+const BASE_URL =
+    "https://raw.githubusercontent.com/Alain31140/Agenda-data/main/";
+
+const DATA_URL =
+    BASE_URL + "json/";
+
+const IMAGE_URL =
+    BASE_URL + "images/";
